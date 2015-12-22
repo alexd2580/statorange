76471cbe5a4b56b014dccb6cb865aa996ac804d0
Alexander Dmitriev <alexander.dmitriev2580@gmail.com>
2015-12-22 06:28:56 +0100

    Date broken

--- src/StateItems/Date.cpp
+++ src/StateItems/Date.cpp
@@ -7,11 +7,9 @@
 
 using namespace std;
 
-string Date::get_date = "";
-
 void Date::settings(JSONObject& section)
 {
-  get_date.assign(section["get_date"].string());
+  //get_date.assign(section["get_date"].string());
 }
 
 Date::Date(JSONObject& item) :
@@ -21,7 +19,7 @@ Date::Date(JSONObject& item) :
 
 bool Date::update(void)
 {
-  time = execute(get_date);
+  //time = execute(get_date); TODO
   time.pop_back(); //date prints newline
   return true;
 }
@@ -32,4 +30,64 @@ void Date::print(void)
   print_icon(icon_clock);
   cout << ' ' << time << ' ';
   separate(Left, white_on_black);
+}
+
+enum WeekDays
+{
+  Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+};
+enum Months
+{
+  January, February, March, April, May, June, July, August, September, October, November, December
+};
+
+namespace Abbr
+{
+  enum WeekDays
+  {
+    Mon, Tue, Wed, Thu, Fri, Sat, Sun
+  };
+  enum Months
+  {
+    Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec
+  };
+}
+
+void Date::parseFormat(string s)
+{
+  tokens.clear();
+  Token token;
+  for(auto i = s.begin(); i != s.end(); i++)
+  {
+    token.type = DateItem;
+    switch(*i)
+    {
+      #define TOKEN(l, a) case l: token.i = a; break;
+      TOKEN('a', AbbrWeekDayName)
+      TOKEN('A', WeekDayName)
+      TOKEN('b', AbbrMonthName)
+      TOKEN('B', MonthName)
+      TOKEN('d', MonthDay)
+      TOKEN('e', MonthDaySpc)
+      TOKEN('H', Hour24)
+      TOKEN('I', Hour12)
+      TOKEN('j', YearDay)
+      TOKEN('k', Hour24Spc)
+      TOKEN('l', Hour12Spc)
+      TOKEN('m', Month)
+      TOKEN('M', Minute)
+      TOKEN('p', AMPM)
+      TOKEN('P', AMPMLow)
+      TOKEN('S', Second)
+      TOKEN('u', WeekDay)
+      TOKEN('W', Week)
+      TOKEN('y', Year2)
+      TOKEN('Y', Year4)
+      default:
+        token.type = Symbol;
+        token.c = *i;
+        break;
+    }
+    tokens.push_back(token);
+  }
 }
--- src/StateItems/Date.hpp
+++ src/StateItems/Date.hpp
@@ -2,17 +2,60 @@
 #define __DATEXYZHEADER_LOL___
 
 #include<string>
+#include<vector>
 #include"../StateItem.hpp"
+
+enum Identifier
+{
+  AbbrWeekDayName,//  a     locale's abbreviated weekday name (e.g., Sun)
+  WeekDayName,    //  A     locale's full weekday name (e.g., Sunday)
+  AbbrMonthName,  //  b     locale's abbreviated month name (e.g., Jan)
+  MonthName,      //  B     locale's full month name (e.g., January)
+  MonthDay,       //  d     day of month (e.g., 01)
+  MonthDaySpc,    //  e     day of month, space padded
+  Hour24,         //  H     hour (00..23)
+  Hour12,         //  I     hour (01..12)
+  YearDay,        //  j     day of year (001..366)
+  Hour24Spc,      //  k     hour, space padded ( 0..23)
+  Hour12Spc,      //  l     hour, space padded ( 1..12)
+  Month,          //  m     month (01..12)
+  Minute,         //  M     minute (00..59)
+  AMPM,           //  p     AM or PM
+  AMPMLow,        //  P     like p, but lower case
+  Second,         //  S     second (00..60)
+  WeekDay,        //  u     day of week (1..7); 1 is Monday
+  Week,           //  W     week number of year, with Monday as first day of week (00..53)
+  Year2,          //  y     last two digits of year (00..99)
+  Year4           //  Y     year
+};
+
+enum TokenType
+{
+  DateItem, Symbol
+};
+
+typedef struct
+{
+  TokenType type;
+  union
+  {
+    Identifier i;
+    char c;
+  };
+} Token;
 
 class Date : public StateItem
 {
 private:
+  std::vector<Token> tokens;
+  void parseFormat(std::string s);
+
   //DATE & TIME
   std::string time;
-  static std::string get_date;
 
   bool update(void);
   void print(void);
+
 public:
   static void settings(JSONObject& section);
   Date(JSONObject& item);
